c62790f13607904d8503b444894a482b3f5207ff
Luka <lukarauter19@gmail.com>
2024-03-25 14:11:47 +0100

    App for avto-servis

---
+++ Program.cs
@@ -0,0 +1,159 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks; 
+
+namespace Avto
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            //"Izjavljam, da sem nalogo opravil samostojno in da sem njen avtor. Zavedam se, da v primeru, če izjava prvega stavka ni resnična, kršim disciplinska pravila."
+            bool izhod = false;
+            while (!izhod)
+            {
+                //IZPIS
+                Console.Clear();
+                Console.WriteLine("Glavni Meni");
+                Console.WriteLine("NAVODILA : vtipkajte številko opcijo katere želite.");
+                Console.WriteLine("1. Dodaj avto");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("2. Natisni inventar");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("3. Shrani inventar (če hočete vozilo shraniti stalno po vnosu izberite to možnost)");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("4. Naloži inventar");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("5. Izbriši vnos");
+                Console.WriteLine("--------------------");
+                Console.WriteLine("6. Izhod");
+                Console.WriteLine("--------------------");
+                Console.Write("Izberi možnost: ");
+                string vnos = Console.ReadLine();
+
+                switch (vnos)
+                {
+                    case "1":
+                        AddCar();
+                        break;
+                    case "2":
+                        PrintInventory();
+                        break;
+                    case "3":
+                        SaveInventory();
+                        break;
+                    case "4":
+                        LoadInventory();
+                        break;
+                    case "5":
+                        DeleteCar();
+                        break;
+                    case "6":
+                        izhod = true;
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+
+                Console.WriteLine("Pritisnite ENTER da nadaljujete");
+                Console.ReadLine();
+            }
+        }
+
+        static void AddCar()
+        {
+            Console.WriteLine("Vpišite značilnosti avta:");
+            Console.Write("Znamka: ");
+            string znamka = Console.ReadLine();
+            if (int.TryParse(znamka, out _)) 
+            {
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+                
+            }
+            else if(char.TryParse(znamka, out _)){
+
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+                return;
+            }
+            Console.Write("Model: ");
+            string model = Console.ReadLine();
+            if (int.TryParse(model, out _))
+            {
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+
+            }
+            else if (char.TryParse(model, out _))
+            {
+
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+                return;
+            }
+            Console.Write("Letnik: ");
+            
+            int letnik = int.Parse(Console.ReadLine());
+            Console.Write("Barva: ");
+            string barva = Console.ReadLine();
+            if (int.TryParse(barva, out _))
+            {
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+
+            }
+            else if (char.TryParse(barva, out _))
+            {
+
+                Console.WriteLine("Napačen format znamke. Prosimo, vnesite veljaven niz.");
+                return;
+            }
+            Console.Write("Cena: ");
+            int cena = int.Parse(Console.ReadLine());
+
+            knjiznica avto = new knjiznica(znamka, model, letnik, barva, cena);
+            ZalogaAvtov.DodajAvto(avto);
+            Console.WriteLine("Avto je bil dodan uspešno");
+        }
+
+        static void PrintInventory()
+        {
+            Console.WriteLine("Trenutni inventorij: ");
+            ZalogaAvtov.NatisniInventuro();
+        }
+
+        static void SaveInventory()
+        {
+            string pot_datoteke = @"C:\Users\lukar\OneDrive\Namizje\Avto\Inventura_Avtov.txt";
+            ZapisovanjePodatkov.NapišiInventuroVDatoteko(ZalogaAvtov.avti, pot_datoteke);
+            Console.WriteLine("Inventorij je bil shranjen uspešno.");
+        }
+
+        static void LoadInventory()
+        {
+            string pot_datoteke = @"C:\Users\lukar\OneDrive\Namizje\Avto\Inventura_Avtov.txt";
+            List<knjiznica> nalozeni_avti = BranjePodatkov.PreberiInventuroIzDatoteke(pot_datoteke);
+            ZalogaAvtov.avti.Clear();
+            ZalogaAvtov.avti.AddRange(nalozeni_avti);
+            Console.WriteLine("Inventorij je bil naložen uspešno");
+        }
+        static void DeleteCar()
+        {
+            Console.WriteLine("Vnesi index od avta katerega hočete zbrisati:");
+            int index = int.Parse(Console.ReadLine());
+
+            if (index >= 0 && index < ZalogaAvtov.avti.Count)
+            {
+                knjiznica car = ZalogaAvtov.avti[index];
+                ZalogaAvtov.OdstraniAvto(car);
+                Console.WriteLine("Avto je bil izbrisan uspešno.");
+            }
+            else
+            {
+                Console.WriteLine("Neveljaven index.");
+            }
+        }
+     
+    }
+}
---
+++ knjiznica.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+
+namespace Avto
+{
+    //"Izjavljam, da sem nalogo opravil samostojno in da sem njen avtor. Zavedam se, da v primeru, če izjava prvega stavka ni resnična, kršim disciplinska pravila."
+    class knjiznica
+    {
+        public string znamka { get; set; }
+        public string model { get; set; }
+        public int letnik { get; set; }
+        public string barva { get; set; }
+        public int cena { get; set; }
+
+        public knjiznica(string znamka, string model, int letnik, string barva, int cena)
+        {
+            this.znamka = znamka;
+            this.model = model;
+            this.letnik = letnik;
+            this.barva = barva;
+            this.cena = cena;
+        }
+    }
+
+    static class ZalogaAvtov
+    {
+        public static List<knjiznica> avti = new List<knjiznica>();
+        //DODAJANJE OBJEKTOV
+        public static void DodajAvto(knjiznica knjiznica)
+        {
+            avti.Add(knjiznica);
+        }
+
+        //BRISANJE OBJEKTOV trenutna inventura
+        public static void OdstraniAvto(knjiznica knjiznica)
+        {
+            avti.Remove(knjiznica);
+        }
+        public static void NatisniInventuro()
+        {
+            foreach (knjiznica knjiznica in avti)
+            {
+                Console.WriteLine("{0} {1} {2} {3} {4}", knjiznica.znamka, knjiznica.letnik, knjiznica.model, knjiznica.barva, knjiznica.cena);
+            }
+        }
+
+    }
+    //ZAPISOVANJE PODATKOV
+    static class ZapisovanjePodatkov
+    {
+        public static void NapišiInventuroVDatoteko(List<knjiznica> avti, string pot_datoteke)
+        {
+            StreamWriter zapisovalnik = new StreamWriter(pot_datoteke);
+            foreach (knjiznica avto in avti)
+            {
+                zapisovalnik.WriteLine("{0} {1} {2} {3} {4}", avto.znamka, avto.letnik, avto.model, avto.barva, avto.cena);
+            }
+            zapisovalnik.Close();
+        }
+    }
+    static class BranjePodatkov
+    {
+        public static List<knjiznica> PreberiInventuroIzDatoteke(string pot_datoteke)
+        {
+            List<knjiznica> avti = new List<knjiznica>();
+            try
+            {
+                using (StreamReader branje = new StreamReader(pot_datoteke))
+                {
+                    string vrstica;
+                    while ((vrstica = branje.ReadLine()) != null)
+                    {
+                        string[] podatki = vrstica.Split(' ');
+                        string znamka = podatki[0];
+                        int letnik = int.Parse(podatki[1]);
+                        string model = podatki[2];
+                        string barva = podatki[3];
+                        int cena = int.Parse(podatki[4]);
+                        avti.Add(new knjiznica(znamka, model, letnik, barva, cena));
+                    }
+                }
+            }
+            catch (FileNotFoundException napaka)
+            {
+                Console.WriteLine("Napaka pri branju datoteke: {0}", napaka.Message);
+            }
+            catch (IOException napaka)
+            {
+                Console.WriteLine("Napaka pri branju podatkov: {0}", napaka.Message);
+            }
+            catch (FormatException napaka)
+            {
+                Console.WriteLine("Napaka pri pretvarjanju podatkov: {0}", napaka.Message);
+            }
+            catch (Exception napaka)
+            {
+                Console.WriteLine("Napaka: {0}", napaka.Message);
+            }
+
+            return avti;
+        }
+    }
+}
+
+
+
+
+
+
